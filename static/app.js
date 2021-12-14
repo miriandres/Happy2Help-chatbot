4ac75bdcb060bc0e4a4e026c5f93f0af38d7cdc6
Miriam Andrés Martín <miriandres.dev@gmail.com>
2021-12-14 19:01:51 +0100

    New JS

--- static/app.js
+++ static/app.js
@@ -1,91 +1,91 @@
-class Chatbox {
-    constructor() {
-        this.args = {
-            openBtn: document.querySelector('.chatbox__btn'),
-            chatBox: document.querySelector('.chatbox__support'),
-            sendBtn: document.querySelector('.send__btn')
+const openBtn      = document.querySelector('.chatbox__btn');
+const chatBox      = document.querySelector('.chatbox__support');
+const sendBtn      = document.querySelector('.send__btn');
+const input        = document.querySelector('input');
+const chatmessage  = document.querySelector('.chatbox__messages');
+const greeting     = document.querySelectorAll('.messages__greeting');
+
+let messages = [];
+let state = false;
+
+
+const display = () => {
+    openBtn.addEventListener('click', () => toggleState())
+    
+    sendBtn.addEventListener('click', () => onSendButton())
+
+    input.addEventListener("keyup", ({key}) => {
+        if (key === "Enter") {
+            onSendButton()
         }
+    })
+}
 
-        this.state = false;
-        this.messages = [];
-    }
+const toggleState = () => {
+    state = !state;
 
-    display() {
-        const {openBtn, chatBox, sendBtn} = this.args;
-
-        openBtn.addEventListener('click', () => this.toggleState(chatBox))
-
-        sendBtn.addEventListener('click', () => this.onSendButton(chatBox))
-
-        const node = chatBox.querySelector('input');
-        node.addEventListener("keyup", ({key}) => {
-            if (key === "Enter") {
-                this.onSendButton(chatBox)
-            }
+    // show or hides the box
+    if(state) {
+        chatBox.classList.add('chatbox--active')
+        greeting.forEach((cadaGreeting, i) => {
+            greeting[i].classList.add('active')
         })
-    }
-
-    toggleState(chatbox) {
-        this.state = !this.state;
-
-        // show or hides the box
-        if(this.state) {
-            chatbox.classList.add('chatbox--active')
-        } else {
-            chatbox.classList.remove('chatbox--active')
-        }
-    }
-
-    onSendButton(chatbox) {
-        var textField = chatbox.querySelector('input');
-        let text1 = textField.value
-        if (text1 === "") {
-            return;
-        }
-
-        let msg1 = { name: "User", message: text1 }
-        this.messages.push(msg1);
-
-        fetch('http://127.0.0.1:5000/predict', {
-            method: 'POST',
-            body: JSON.stringify({ message: text1 }),
-            mode: 'cors',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-          })
-          .then(r => r.json())
-          .then(r => {
-            let msg2 = { name: "Happy2Help", message: r.answer };
-            this.messages.push(msg2);
-            this.updateChatText(chatbox)
-            textField.value = ''
-
-        }).catch((error) => {
-            console.error('Error:', error);
-            this.updateChatText(chatbox)
-            textField.value = ''
-          });
-    }
-
-    updateChatText(chatbox) {
-        var html = '';
-        this.messages.slice().reverse().forEach(function(item, index) {
-            if (item.name === "Happy2Help")
-            {
-                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>'
-            }
-            else
-            {
-                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>'
-            }
-          });
-
-        const chatmessage = chatbox.querySelector('.chatbox__messages');
-        chatmessage.innerHTML = html;
+    } else {
+        chatBox.classList.remove('chatbox--active')
+        greeting.forEach((cadaGreeting, i) => {
+            greeting[i].classList.remove('active')
+        })
     }
 }
 
+const onSendButton = () => {
+    let text1 = input.value
+    if (text1 === "") {
+        return;
+    }
 
-const chatbox = new Chatbox();
-chatbox.display();
+    let msg1 = { name: "User", message: text1 }
+    messages.push(msg1);
+
+    fetch('http://127.0.0.1:5000/predict', {
+        method: 'POST',
+        body: JSON.stringify({ message: text1 }),
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+      .then(r => r.json())
+      .then(r => {
+        updateChatText(text1, r.answer);
+        input.value = ''
+
+    }).catch((error) => {
+        console.error('Error:', error);
+        updateChatText()
+        input.value = ''
+      });
+}
+
+const updateChatText = (input, bot) => {
+    
+    let userDiv = document.createElement("div");
+        userDiv.className = "messages__item messages__item--operator";
+        userDiv.innerHTML = `${input}`;
+        chatmessage.appendChild(userDiv);
+   
+    let botDiv = document.createElement("div");
+        botDiv.className = "messages__item messages__item--visitor";
+        botDiv.innerHTML = `<div class="loader">
+                                <div class="loader__dot"></div>
+                                <div class="loader__dot"></div>
+                                <div class="loader__dot"></div>
+                            </div>`;                  
+        chatmessage.appendChild(botDiv);
+                              
+    setTimeout(() => {
+      botDiv.innerText = `${bot}`;
+    }, 1500); 
+}
+
+display();
