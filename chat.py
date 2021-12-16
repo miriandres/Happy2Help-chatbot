e29a17862b99640478cd52fc3314f64ec721aae0
Jesica Míguez Rosas <jmezrosas@gmail.com>
2021-12-16 11:35:51 +0100

    Include choice pattern for chatbot responses

--- chat.py
+++ chat.py
@@ -2,6 +2,7 @@ import random
 import json
 
 import torch
+import numpy as np
 
 from model import NeuralNet
 from nltk_utils import bag_of_words, codificacion_sentence, stopwords, tokenize, padding
@@ -56,25 +57,60 @@ def get_response(msg):
     # Predicción Keras
     # Valor entre 0 y 1
     output_sentimientos = model_sentiments.predict(padding(X_sentimientos))
+    input_score = output_sentimientos[0]
+    #If the model can`t recognize the input, set the value to 0
+    non_valid_input_score = [0]
 
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
-    if prob.item() > 0.75:
-        for intent in intents['intents']:
-            if tag == intent["tag"]:
-                return random.choice(intent['responses'])
+    pred_item = prob.item()
     
-    return "Not sure I understand"
+    #Change tags in order to lead the conversation
+    if len(score) == 1:
+        tag = 'interest'
+    elif len(score) == 2:
+        tag = 'time'
+    #Release the direction of the speech, so user can access more conversation options or finish it anytime
+    elif len(score) >= 3 and pred_item < 0.75 or len(score) >=3 and tag == 'about':
+        #Tag-about prevents bot to redirect conversation to greeting phase if 'day' is used
+        if tag == 'about':
+            input_score = [0]
+        score.pop(0)
+        average = np.average(score)
+        print(average)
+        if average < 0.2:
+            tag = 'joy'
+        else:
+            tag = 'support'
+
+    #Select an answer for our bot
+    if prob.item() > 0.75:
+        # Return a tuple (response + user input input_score)
+        for intent in intents['intents']:
+            if tag == intent['tag']:
+                return random.choice(intent['responses']), input_score
+
+    return "Not sure I understand", non_valid_input_score
 
 
 if __name__ == "__main__":
-    print("Hi! How are you feeling right now? (type 'quit' to exit)")
+    print(random.choice(intents['intents']['tag'=='greeting']['responses']))
+    #print("Hi! How are you feeling right now? (type 'quit' to exit)")
+    # Declare an array to keep track on user reponses score
+    score_resp = []
+    
     while True:
         # sentence = "I feel sad"
         sentence = input("You: ")
         if sentence == "quit":
             break
 
-        resp = get_response(sentence)
+        answer = get_response(sentence, score_resp)
+        #Bot response will be the first part of the tuple provided by the 'get_response' function
+        resp = answer[0]
+        #Add the score to a previous array
+        score_resp.append(answer[1][0])
+        
         print(resp)
+        print(score_resp)
 
